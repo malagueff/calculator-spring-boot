f5473b9d4071b4dcb891db0b519c236281329b46
Jose David Garcia Lopez <jdgarcial@minsait.com>
2022-02-01 22:08:30 +0100

    Creacion base proyecto con spring boot initializer

---
+++ src/main/java/com/architecture/calculatorspringboot/CalculatorSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.calculatorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/architecture/calculatorspringboot/CalculatorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.calculatorspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
